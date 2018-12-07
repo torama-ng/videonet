57acadec63420d73e818ae1afd326ec5e0280b6f
Heavens <ebiakpoheavens@gmail.com>
2018-12-07 08:25:23 -0800

    Side bar Menu updated

--- heavens/nodejs/public/javascripts/misc.js
+++ heavens/nodejs/public/javascripts/misc.js
@@ -1,3 +1,4 @@
+var c = 0;
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -24,30 +25,33 @@ function checkTime(i) {
 }
 
 function openNav() {
+  c++
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0,4)";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.1)";
+  if(c%2==0) {
+    document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-
   document.body.style.backgroundColor = "white";
-}
-
-function myFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-          li[i].style.display = "";
-      } else {
-          li[i].style.display = "none";
-      }
   }
 }
+
+// function closeNav() {
+  
+// }
+
+// function myFunction() {
+//   var input, filter, ul, li, a, i;
+//   input = document.getElementById("mySearch");
+//   filter = input.value.toUpperCase();
+//   ul = document.getElementById("myMenu");
+//   li = ul.getElementsByTagName("li");
+//   for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//           li[i].style.display = "";
+//       } else {
+//           li[i].style.display = "none";
+//       }
+//   }
+// }
