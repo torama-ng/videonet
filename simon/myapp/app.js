6e3f8d09294ee55b97bae1cf44b66217aad985e5
Simon Jerry <swagasoft@gmail.com>
2018-12-17 16:21:15 +0100

    mongodb added to project

--- simon/myapp/app.js
+++ simon/myapp/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let expressValidator = require('express-validator');
+let mongo = require('mongodb').MongoClient;
 const walk = require('./walk.js');
 let bodyParser = require ('body-parser');
 
