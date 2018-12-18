c5bc46a6350b0ce681fa1580c20655a6842a4932
Filatei <filatei@gtsng.com>
2018-12-18 01:11:05 +0100

    lesson 3 flexbox

---
+++ Lessons/3_css_flexbox/routes/handlebars.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const walk = require('../walk.js');
+console.log(__dirname);
+
+var walkSync = [];
+walkSync = walk.walkSync('videos/handlebars');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('view', { 
+    videoTitle: 'Handlebars Videos',
+    videoFiles: walkSync,
+    videoDir: 'Handlebars'
+
+  });
+});
+
+module.exports = router;
