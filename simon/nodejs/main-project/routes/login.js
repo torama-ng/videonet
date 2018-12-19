81cd6fdacc7a318931ca6a418890345eb0e75024
Simon Jerry <swagasoft@gmail.com>
2018-12-19 07:11:35 +0100

    user login successful

--- simon/nodejs/main-project/routes/login.js
+++ simon/nodejs/main-project/routes/login.js
@@ -1,33 +1,35 @@
 const express = require('express');
-var busboy = require('connect-busboy');
-var bodyParser = require('body-parser');
+let busboy = require('connect-busboy');
+const bodyParser = require('body-parser');
 const router = express.Router();
 //  implementation of mongoDB
 let mongodb = require('mongodb');
 
+let app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 
 router.get('/', function(req, res,next) {
 
 
-    var MongoClient = mongodb.MongoClient;
+    let MongoClient = mongodb.MongoClient;
 
-    var url = "mongodb://localhost:27017/userDB";
+    let url = "mongodb://localhost:27017/userDB";
 
     MongoClient.connect(url, (error,db) =>{
         if(error) throw error;
 
         console.log('connection to mongodb successful');
 
-        var dbo = db.db('userDB');
+        let dbo = db.db('userDB');
 
-        var collection = dbo.collection('userDetails');
+        let collection = dbo.collection('userDetails');
 
         collection.find({}).toArray(function(err,result){
                 if(err) throw err;
 
                 else if(result.length){
-                    console.log(result);
+                   // console.log(result);
                 }
         });
         db.close();
@@ -36,6 +38,11 @@ router.get('/', function(req, res,next) {
     res.render( 'registerview');
   
 });
+
+router.post('/',function(req,res){
+    console.log(req.body);
+   res.send('registration successful');
+})
 
 
 
