3498748e11369687ee789a8a53535ca05c95fe11
Heavens <ebiakpoheavens@gmail.com>
2019-01-08 11:54:17 -0800

    video title hyperlinked to playarea using hbs helpers with four videos per category but no scroll function yet

--- heavens/hbs/routes/index.js
+++ heavens/hbs/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const walk = require('../walk.js');
+console.log(__dirname);
+var walkSync = [];
+walkSync = walk.walkSync('videos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    videoTitle: 'Nodejs Videos',
+    videoFiles: walkSync,
+    videoDir: 'Nodejs'
+
+  });
 });
 
 module.exports = router;
