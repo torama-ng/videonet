3a396fe1a85f616f73895d8077e259f327d592f6
alabo <festusalabo@gmail.com>
2019-01-11 12:27:20 +0100

    updating navigation bar and carousels

--- alabo/routes/javasc.js
+++ alabo/routes/javasc.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const walk = require('../walk.js');
-console.log(__dirname);
+const scan = require('../scan.js');
 
-var walkSync = [];
-walkSync = walk.walkSync('videos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('videos', {
-        videoTitle: 'Video Files',
-        videoFiles: walkSync,
+        videoTitl: 'javascript Files',
+        javascriptfiles: walkSync,
         videoDir: 'videos'
 
     });
