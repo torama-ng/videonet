c065d6655f1661facf910ef2188328bed018196b
Ebikon Felicia <ebikonfelicia@yahoo.com>
2019-01-11 11:22:08 +0100

    video carousel added

---
+++ Felicia/sitevid/routes/vcarousel.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const walk = require('../walk.js');
+console.log(__dirname);
+
+var walkSync = [];
+walkSync = walk.walkSync('vcarousel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('view', { 
+    videoTitle: 'Carousel Videos',
+    videoFiles: walkSync,
+    videoDir: 'vcarousel'
+
+  });
+});
+
+
+
+module.exports = router;
