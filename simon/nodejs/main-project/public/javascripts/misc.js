29b5e872845ec834b1b6cb37eb5e6400f640c67f
Simon Jerry <swagasoft@gmail.com>
2019-01-18 11:35:34 +0100

    project update, play area to display videos

--- simon/nodejs/main-project/public/javascripts/misc.js
+++ simon/nodejs/main-project/public/javascripts/misc.js
@@ -34,5 +34,73 @@ class Logger extends EventEmitter{
   }
 }
 
+function setDuration(indx) {  
+ 
+       
+   var dur = document.getElementById('video-'+ indx).duration;
+   duration = (dur/60).toFixed(2);
+   
+   document.getElementById('vid-'+ indx).textContent = duration + " mins";
+ 
+ }
+ 
+ function playVideo(selectid) {
+   let video = "";
+   
+   video = document.getElementById('videoid');
+   
+   let videotext = document.getElementById('videotitle');
+   let folder = document.querySelector('select').id;
+   
+   let selected = selectid.value;
+  
+   videotext.innerText = selected.substring(0, selected.lastIndexOf('.'));
+   selected = '/' + folder + '/' + selected;
+  
+   console.log(`selected id is ${selected} `); 
+ 
+   video.src = selected;
+  
+   video.load();
+   
+   // document.getElementById(selectid).onclick = function(){
+   video.play();
+   let dur = video.duration;
+   document.getElementById('vid-0').textContent = (dur/60).toFixed(0) + " mins";
+   
+   console.log(`current video url is ${video.currentSrc}`);
+   // }
+   
+ }
+ 
+ 
+ function playcVideo(selectid) {
+   let video = "";
+   console.log(`play value is ${selectid}`);
+ 
+   video = document.getElementById('videoid');
+   
+   let videotext = document.getElementById('videotitle');
+   let folder = document.querySelector('h5').id;
+   
+   let selected = selectid;
+   
+   videotext.innerText = selected.substring(0, selected.lastIndexOf('.'));
+   selected = '/' + folder + '/' + selected;
+ 
+   var source = document.createElement('source');
+   
+ 
+   video.src = encodeURI(selected);
+  
+   video.load();
+   
 
+   video.play();
+   let dur = video.duration;
+   document.getElementById('vid-0').textContent = (dur/60).toFixed(0) + " mins";
+   document.getElementById('videoid').focus();
+  
+ }
+ 
 
